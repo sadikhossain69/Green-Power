4e90b5bba9f40d42885fcf6f43910ee6038024dc
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-21 20:27:35 +0600

    what we do 50% done

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import AboutUs from "@/components/pages/home/AboutUs/AboutUs";
 import HeroSection from "@/components/pages/home/HeroSection/HeroSection";
+import WhatWeDo from "@/components/pages/home/WhatWeDo/WhatWeDo";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutUs />
+      <WhatWeDo />
     </main>
   )
 }
---
+++ components/pages/home/WhatWeDo/WhatWeDo.js
@@ -0,0 +1,21 @@
+const WhatWeDo = () => {
+    return (
+        <>
+            <section>
+                <div>
+                    <p className="text-[#0000FF] text-3xl text-center font-semibold">What do we do?!</p>
+                </div>
+                <div className=" relative container mx-auto flex flex-col justify-between items-center lg:flex-row py-10">
+                    <img src="whatWeDoLeft.jpg" alt="" />
+                    <p className="text-[#0000FF] font-semibold text-center text-6xl">
+                        Give us a call to get started
+                    </p>
+                    <div className="hidden xl:block absolute bottom-28 left-[35%] w-96 border-b-2 border-[#52FF00]"></div>
+                    <img src="whatWeDoRight.jpg" alt="" />
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default WhatWeDo;
