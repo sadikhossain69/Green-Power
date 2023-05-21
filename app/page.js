81b8638b772d65fc1d2a5d6fb00a916b37a9e17a
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-21 19:35:49 +0600

    about us section done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import AboutUs from "@/components/pages/home/AboutUs/AboutUs";
 import HeroSection from "@/components/pages/home/HeroSection/HeroSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <AboutUs />
     </main>
   )
 }
