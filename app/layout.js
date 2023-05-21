c349bd50c5e44be2461c8f33646fe2c837eeef8c
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-22 00:10:28 +0600

    previous font remove

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
 import Navbar from '@/components/layout/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Footer from '@/components/layout/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Green Powered Systems LTD',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
         <Footer />
