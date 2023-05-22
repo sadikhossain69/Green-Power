d260065f21db3a899cc56493fa59a684801ad6cf
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-23 03:41:15 +0600

    some changes to solve issue

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+"use client";
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
 import AboutUs from "@/components/pages/home/AboutUs/AboutUs";
 import BigImage from "@/components/pages/home/BigImage/BigImage";
 import ContactAddress from "@/components/pages/home/ContactAddress/ContactAddress";
@@ -5,17 +8,38 @@ import HeroSection from "@/components/pages/home/HeroSection/HeroSection";
 import LogoSlider from "@/components/pages/home/LogoSlider/LogoSlider";
 import OurServices from "@/components/pages/home/OurServices/OurServices";
 import WhatWeDo from "@/components/pages/home/WhatWeDo/WhatWeDo";
+import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function Home() {
+
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, [3000]);
+  }, []);
+
+
   return (
     <main>
-      <HeroSection />
-      <AboutUs />
-      <WhatWeDo />
-      <OurServices />
-      <LogoSlider />
-      <BigImage />
-      <ContactAddress />
+      {
+        loading ? <Loading /> :
+          <>
+            <Navbar />
+            <HeroSection />
+            <AboutUs />
+            <WhatWeDo />
+            <OurServices />
+            <LogoSlider />
+            <BigImage />
+            <ContactAddress />
+            <Footer />
+          </>
+      }
     </main>
   )
 }
