5961314286604ed6b0985598c3b3fc0bdf4513de
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-23 03:43:43 +0600

    issue solving

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import Navbar from '@/components/layout/Navbar'
 import './globals.css'
 import Footer from '@/components/layout/Footer'
-import { Suspense, useEffect, useState } from 'react'
-import Loading from './loading'
 
 export const metadata = {
   title: 'Green Powered Systems LTD',
